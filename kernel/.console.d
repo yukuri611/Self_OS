186771adb38dee786df860994713920ff288b7f1
yukuri611 <yukiiw611@gmail.com>
2025-04-13 21:07:17 +0900

    create printk func(day05f)

---
+++ kernel/.console.d
@@ -0,0 +1,7 @@
+console.o: console.cpp console.hpp graphics.hpp frame_buffer_config.hpp \
+  /home/yukiiwahashi/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
+  /home/yukiiwahashi/osbook/devenv/x86_64-elf/include/c++/v1/__config \
+  /home/yukiiwahashi/osbook/devenv/x86_64-elf/include/c++/v1/cstring \
+  /home/yukiiwahashi/osbook/devenv/x86_64-elf/include/c++/v1/string.h \
+  font.hpp \
+  /home/yukiiwahashi/osbook/devenv/x86_64-elf/include/c++/v1/cstdint
--- kernel/.main.d
+++ kernel/.main.d
@@ -6,4 +6,4 @@ main.o: main.cpp \
   /home/yukiiwahashi/osbook/devenv/x86_64-elf/include/c++/v1/version \
   /home/yukiiwahashi/osbook/devenv/x86_64-elf/include/c++/v1/cstdio \
   /home/yukiiwahashi/osbook/devenv/x86_64-elf/include/c++/v1/stdio.h \
-  frame_buffer_config.hpp graphics.hpp font.hpp
+  frame_buffer_config.hpp graphics.hpp font.hpp console.hpp
