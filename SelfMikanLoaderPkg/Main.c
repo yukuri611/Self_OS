6abb8e948c3ce715414f113c3ae618268404548d
yukuri611 <yukiiw611@gmail.com>
2025-04-13 09:11:14 +0900

    draw pixel from kernel(not yet able to load kernel until day04d) (day03c)

--- SelfMikanLoaderPkg/Main.c
+++ SelfMikanLoaderPkg/Main.c
@@ -235,11 +235,10 @@ EFI_STATUS EFIAPI UefiMain(
     //call kernel
     UINT64  entry_addr = *(UINT64*)(kernel_base_addr + 24);
 
-    typedef void EntryPointType(void);
+    typedef void EntryPointType(UINT64, UINT64);
     EntryPointType* entry_point = (EntryPointType*)entry_addr;
-    entry_point();
-
-
+    entry_point(gop->Mode->FrameBufferBase, gop->Mode->FrameBufferSize);
+    
     Print(L"All done\n");
 
     while (1);
