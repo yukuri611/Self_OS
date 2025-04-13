f0be732108f7e0c2ba55584d8bf53c5af006c72a
yukuri611 <yukiiw611@gmail.com>
2025-04-13 11:53:13 +0900

    カーネルのloadingが正しく行われるようbootloaderのプログラムに変更を加えた。また、カーネル側からピクセルの変更を行えるようにした(day04b + day04d)

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,10 +1,45 @@
 #include <cstdint>
+#include <cstddef>
+#include "frame_buffer_config.hpp"
 
-extern "C" void KernelMain(uint64_t framebuffer_base, 
-                           uint64_t frame_buffer_size) {
-    uint8_t* frame_buffer = reinterpret_cast<uint8_t*>(framebuffer_base);
-    for (uint64_t i = 0; i < frame_buffer_size; i++) {
-        frame_buffer[i] = i % 256;
+struct PixelColor {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+};
+
+int WritePixel(const FrameBufferConfig& config, int x, int y, const PixelColor& c) {
+    const int pixel_position = config.pixels_per_scan_line * y + x;
+    if (config.pixel_format == kPixelRGBResv8BitPerColor) {
+        uint8_t* p = &config.frame_buffer[4 * pixel_position];
+        p[0] = c.r;
+        p[1] = c.g;
+        p[2] = c.b;
     }
-    while (1) __asm__("hlt");
+    else if (config.pixel_format == kPixelBGRResv8BitPerColor) {
+        uint8_t* p = &config.frame_buffer[4*pixel_position];
+        p[0] = c.b;
+        p[1] = c.g;
+        p[2] = c.r;
+    }
+    else {
+        return -1; // Invalid pixel format
+    }
+    return 0;
+}
+
+extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
+    for (int x = 0; x < frame_buffer_config.horizontal_resolution; ++x) {
+        for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y) {
+            WritePixel(frame_buffer_config , x, y, {100,100,100});
+        }
+    }
+    for (int x = 0; x < 100; ++x) {
+        for (int y = 0; y < 100; ++y) {
+            WritePixel(frame_buffer_config , x, y, {0,0,0});
+        }
+    }
+    while (1) {
+        __asm__("hlt");
+    }
 }
