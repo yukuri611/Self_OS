b437371bd5fde1596396462bfd86968bffb6b3d0
yukuri611 <yukiiw611@gmail.com>
2025-04-15 21:54:59 +0900

    made interruption process faster(day07b)

--- kernel/.main.d
+++ kernel/.main.d
@@ -48,4 +48,4 @@ main.o: main.cpp \
   usb/classdriver/base.hpp usb/xhci/xhci.hpp usb/xhci/registers.hpp \
   register.hpp usb/xhci/context.hpp usb/xhci/ring.hpp usb/xhci/trb.hpp \
   usb/xhci/port.hpp usb/xhci/devmgr.hpp usb/xhci/device.hpp \
-  interrupt.hpp asmfunc.h
+  interrupt.hpp asmfunc.h queue.hpp
