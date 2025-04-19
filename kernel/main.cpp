cdeb90d54f83a4bea9115582090c8bb01d6bac06
yukuri611 <yukiiw611@gmail.com>
2025-04-19 17:03:52 +0900

    show loop count on main window(day10c)

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -276,12 +276,9 @@ extern "C" void KernelMainNewStack(
     mouse_position = {200, 200};
 
     auto main_window = std::make_shared<Window>(
-        160, 68, frame_buffer_config.pixel_format);
+        160, 52, frame_buffer_config.pixel_format);
     DrawWindow(*main_window->Writer(), "Hello, window!");
-    WriteString(*main_window->Writer(), {24,28}, "Welcome from", {0, 0, 0});
-    WriteString(*main_window->Writer(), {24,44}, "yukuri611!", {0, 0, 0});
     
-
 
     FrameBuffer screen;
     if (auto err = screen.Initialize(frame_buffer_config)) {
@@ -312,11 +309,22 @@ extern "C" void KernelMainNewStack(
     // Draw the layers to the screen in order of heights
     layer_manager->Draw();
 
+    char str[128];
+    unsigned int count = 0;
+
+
 
     while (true) {
+        ++count;
+        sprintf(str, "%010u", count);
+        FillRectangle(*main_window->Writer(), {24, 28}, {8 * 10, 16}, {0xc6, 0xc6, 0xc6});
+        WriteString(*main_window->Writer(), {24, 28}, str, {0, 0, 0});
+        layer_manager->Draw();
+
+        
         __asm__("cli");
         if (main_queue.Count() == 0) {
-            __asm__("sti\n\thlt");
+            __asm__("sti");
             continue;
         }
 
