cc0cfeb4248828505f431ac6737c8de6107481b6
yukuri611 <yukiiw611@gmail.com>
2025-04-21 22:08:28 +0900

    create draggable flag and made only main window draggable(day10g)

--- kernel/layer.hpp
+++ kernel/layer.hpp
@@ -13,14 +13,17 @@ class Layer {
         Layer& SetWindow(const std::shared_ptr<Window>& window);
         std::shared_ptr<Window> GetWindow() const;
         Vector2D<int> GetPosition() const;
+        Layer& SetDraggable(bool draggable);
+        bool IsDraggable() const;
         Layer& Move(Vector2D<int> pos);
         Layer& MoveRelative(Vector2D<int> pos_diff);
         void DrawTo(FrameBuffer& screen, const Rectangle<int>& area) const;
-    
+
     private:
         unsigned int id_;
         Vector2D<int> pos_;
         std::shared_ptr<Window> window_;
+        bool draggable_{false};
 };
 
 class LayerManager {
